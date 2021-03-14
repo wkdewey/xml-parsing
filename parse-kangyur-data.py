12c7f1b1edfb3b1cf245675acd15f90b90520b65
wkdewey <wkdewey@gmail.com>
2021-03-14 14:30:31 -0400

    add importing of xml file and spreadsheet with pandas

--- parse-kangyur-data.py
+++ parse-kangyur-data.py
@@ -1,10 +1,19 @@
 #import the necessary modules
 import xml.etree.ElementTree as ET
+import pandas as pd
+from pathlib import Path
 #load the XML file/copy the file?
 tree = ET.parse('sample-data.xml')
 root = tree.getroot()
+#test
+for child in root:
+    print(child.tag, child.attrib)
 #import the BDRC spreadsheet
-
+spreadsheet = Path("../data_export/Tentative template.xlsx")
+kangyur_sheet = ""
+if spreadsheet.exists():
+  kangyur_sheet = pd.read_excel(spreadsheet, sheet_name = "dergeKangyur")
+print(kangyur_sheet)
 #iterate through XML entries (texts)
 
 #get ID from spreadsheet
