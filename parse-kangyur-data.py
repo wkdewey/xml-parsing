ac87c960be0cee8b1832d0544c2b15f1d4193c81
wkdewey <wkdewey@gmail.com>
2021-03-15 10:43:05 -0400

    add method to find tohoku numbers, add notes on tohoku numbers

--- parse-kangyur-data.py
+++ parse-kangyur-data.py
@@ -1,13 +1,14 @@
 #import the necessary modules
 import xml.etree.ElementTree as ET
 import pandas as pd
+import pdb
 from pathlib import Path
 #load the XML file/copy the file?
 tree = ET.parse('sample-data.xml')
 root = tree.getroot()
 #test
-for child in root:
-    print(child.tag, child.attrib)
+# for child in root:
+#     print(child.tag, child.attrib)
 #import the BDRC spreadsheet
 spreadsheet = Path(__file__).parent / "/Users/williamdewey/Development/code/84000-data-rdf/data-export/Tentative template.xlsx"
 kangyur_sheet = ""
@@ -15,7 +16,14 @@ if spreadsheet.exists():
   kangyur_sheet = pd.read_excel(spreadsheet, sheet_name = "DergeKangyur")
 # print(kangyur_sheet["ID"])
 #iterate through XML entries (texts)
-
+# for label in root.iter('label'):
+#     pdb.set_trace()
+#     print(label)
+#     toh_num = label.textroo
+#     print(toh_num)
+for bibl in root.iter("{http://read.84000.co/ns/1.0}bibl"):
+   toh_num = bibl.attrib["key"]
+  
 #get ID from spreadsheet
 
 #match with ID in this doc
---
+++ tohoku-numbers.md
@@ -0,0 +1,8 @@
+toh1-1 rab tu ’byung ba’i gzhi not found
+toh1-2
+toh1-3
+toh1-4
+toh1-5
+In fact these look like subsections so may be an edge case (?)
+
+toh2 so sor thar pa'i mdo D2; this is the same
