344549d5307b00d620f4b41713a9bea674d7533e
wkdewey <wkdewey@gmail.com>
2021-03-16 15:04:17 -0400

    add ns hash, find roles and names from spreadsheet and work from xml

--- parse-kangyur-data.py
+++ parse-kangyur-data.py
@@ -16,16 +16,27 @@ if spreadsheet.exists():
   kangyur_sheet = pd.read_excel(spreadsheet, sheet_name = "DergeKangyur")
 #iterate through XML entries (texts)
  #should refactor with namespace dictionaries
-for bibl in root.iter("{http://read.84000.co/ns/1.0}bibl"):
+ns = {
+  'default': "http://read.84000.co/ns/1.0",
+  'rdf': "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+  'owl': "http://www.w3.org/2002/07/owl#"
+}
+for text in root.findall("default:text", ns):
+    bibl = text.find("default:bibl", ns)
     toh_num = bibl.attrib["key"][3:]
-    print(toh_num)
     #match ID with spreadsheet
     spread_num = "D" + toh_num
     match = kangyur_sheet.loc[kangyur_sheet["ID"] == spread_num]
-    print(match)
+    #add roles from spreadsheet
+    #get lists of roles, and lists of names
+    roles = match["role"]
+    names = match["indicated value"]
+    #add an element <attribution> under <work>, with role set to value of spreadsheet
+    work = bibl.find("./{http://read.84000.co/ns/1.0}work[@type='tibetanSource']")
+    
+    #add a label with
 
 
-#add roles from spreadsheet
 
 #some query to get associated places, likely from BDRC
 
