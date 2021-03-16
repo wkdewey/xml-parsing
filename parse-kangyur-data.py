3945354ea1967fd96890bc81cd0a076303621140
wkdewey <wkdewey@gmail.com>
2021-03-16 12:49:23 -0400

    delete unused code

--- parse-kangyur-data.py
+++ parse-kangyur-data.py
@@ -1,33 +1,25 @@
-#import the necessary modules
+
 import xml.etree.ElementTree as ET
 import pandas as pd
 import pdb
 import re
 from pathlib import Path
-#load the XML file/copy the file?
+
 data_file = 'sample-data.xml'
 # data_file = "/Users/williamdewey/Development/code/84000-data-rdf/data-export/kangyur-data.xml"
+#load the xml file
 tree = ET.parse(data_file)
 root = tree.getroot()
-#test
-# for child in root:
-#     print(child.tag, child.attrib)
 #import the BDRC spreadsheet
 spreadsheet = Path(__file__).parent / "/Users/williamdewey/Development/code/84000-data-rdf/data-export/Tentative template.xlsx"
 kangyur_sheet = ""
 if spreadsheet.exists():
   kangyur_sheet = pd.read_excel(spreadsheet, sheet_name = "DergeKangyur")
-# print(kangyur_sheet["ID"])
 #iterate through XML entries (texts)
-# for label in root.iter('label'):
-#     pdb.set_trace()
-#     print(label)
-#     toh_num = label.textroo
-#     print(toh_num)
+ #should refactor with namespace dictionaries
 for bibl in root.iter("{http://read.84000.co/ns/1.0}bibl"):
     toh_num = bibl.attrib["key"]
-  
-#get ID from spreadsheet
+    #get ID from spreadsheet
 
 #match with ID in this doc
 
