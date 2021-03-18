b9555fe5115e6eb12035ef3fb3709bc5f93ec358
wkdewey <wkdewey@gmail.com>
2021-03-18 11:11:23 -0400

    begin new strategy of finding possible individuals

--- parse-kangyur-data.py
+++ parse-kangyur-data.py
@@ -29,26 +29,30 @@ ns = {
 for text in root.findall("default:text", ns):
     bibl = text.find("default:bibl", ns)
     toh_num = bibl.attrib["key"][3:]
-    #match ID with spreadsheet
+    #find toh_num in spreadsheet
     spread_num = "D" + toh_num
     match = kangyur_sheet.loc[kangyur_sheet["ID"] == spread_num]
     #add roles from spreadsheet
     #get lists of roles, and lists of names
+    person_ids = match["identification"]
     roles = match["role"]
     names = match["indicated value"]
-    ids = match["identification"]
-    #add an element <attribution> under <work>, with role set to value of spreadsheet
-    work = bibl.find("./{http://read.84000.co/ns/1.0}work[@type='tibetanSource']")
-    for (idx, role) in enumerate(roles):
-      attribution = ET.SubElement(work, "attribution")
-      attribution.attrib["role"] = role
-      #add a label with corresponding name
-      label = ET.SubElement(attribution, "label")
-      label.text = names.iloc[idx]
-      sameAs= ET.SubElement(attribution, "owl:sameAs")
-      if type(ids.iloc[idx]) is str:
-          person_uri = "http://purl.bdrc.io/resource/" + ids.iloc[idx]
-      sameAs.attrib["rdf:resource"] = person_uri
+    possible_individuals = []
+    for (idx, id) in enumerate(person_ids):
+      name = names.iloc[idx]
+      possible_individuals.append((id, name))
+
+    # work = bibl.find("./{http://read.84000.co/ns/1.0}work[@type='tibetanSource']")
+    # for (idx, role) in enumerate(roles):
+    #   attribution = ET.SubElement(work, "attribution")
+    #   attribution.attrib["role"] = role
+    #   #add a label with corresponding name
+    #   label = ET.SubElement(attribution, "label")
+    #   label.text = names.iloc[idx]
+    #   sameAs= ET.SubElement(attribution, "owl:sameAs")
+    #   if type(ids.iloc[idx]) is str:
+    #       person_uri = "http://purl.bdrc.io/resource/" + ids.iloc[idx]
+    #   sameAs.attrib["rdf:resource"] = person_uri
     
 
 
